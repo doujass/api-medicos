7cab932cf901d498a6fe87c261879824e516337b
Douglas de Oliveira Augusto <douglas13.augusto@hotmail.com>
2022-11-25 14:02:29 -0300

    Criação e finalização da api para realizar crud simples.

--- escola/src/main/java/br/com/alura/escola/dominio/aluno/AlunoNãoEncontrado.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.escola.dominio.aluno;
-
-public class AlunoNãoEncontrado extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public AlunoNãoEncontrado(CPF cpf) {
-        super("Aluno não encontrado com CPF" + cpf.getNumero());
-    }
-}
